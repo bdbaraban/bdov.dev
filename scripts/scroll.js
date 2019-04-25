59784f6022cf3fa53d0901ff97302484ba96aa03
bdbaraban <375@holbertonschool.com>
2019-04-25 23:57:42 +0000

    Upload scripts folder with JS progress bar script

---
+++ scripts/scroll.js
@@ -0,0 +1,11 @@
+var h = document.documentElement,
+  b = document.body,
+  st = 'scrollTop',
+  sh = 'scrollHeight',
+  progress = document.querySelector('.progress'),
+  scroll;
+
+document.addEventListener('scroll', function() {
+  scroll = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+  progress.style.setProperty('--scroll', scroll + '%');
+});
