2493deaf0dd2b309f733d5265e764ed2e6f9e34e
bdbaraban <375@holbertonschool.com>
2019-05-21 00:05:45 -0700

    Upload redesigned Bulma project

---
+++ _javascript/dynamic.js
@@ -0,0 +1,95 @@
+/* Icon column slide-in-and-out functionality */
+function reveal(slider) {
+  $(slider).find('div.reveal').show(1250);
+}
+function hide(slider) {
+  $(slider).find('div.reveal').hide(1250);
+}
+function revealThenHide(slider) {
+  async function first() {
+    reveal(slider);
+  }
+  first().then(setTimeout(() => {
+    hide(slider);
+  }, 2000));
+}
+
+/* Automatically slide-out icons upon first scrolling to page */
+const revealed = {
+  '#work-slider': false,
+  '#blog-slider': false,
+  '#social-slider': false,
+};
+function revealOnScroll(sliderId) {
+  $(window).on('scroll', () => {
+    const screenHeight = $(window).height();
+    const scrollPos = window.pageYOffset;
+    const inView = scrollPos > $(sliderId).offset().top - (screenHeight * 0.5);
+    if (!revealed[sliderId] && inView) {
+      setTimeout(() => {
+        revealThenHide($(sliderId));
+      }, 700);
+      revealed[sliderId] = true;
+    }
+  });
+}
+
+$('document').ready(() => {
+  /* Scroll progress bar */
+  const h = document.documentElement;
+  const b = document.body;
+  const progress = document.querySelector('.scroll');
+  document.addEventListener('scroll', () => {
+    const place = (h.scrollTop || b.scrollTop);
+    const height = ((h.scrollHeight || b.scrollHeight) - h.clientHeight);
+    const scroll = place / height * 100;
+    progress.style.setProperty('--scroll', `${scroll}%`);
+  });
+
+  /* Initial title slide-in animation */
+  async function first() {
+    setTimeout(() => {
+      $('h1.entry-slide').show(1250);
+    }, 1500);
+  }
+  first().then(setTimeout(() => {
+    $('h1.entry-slide').hide(1250);
+  }, 4500));
+
+  /* Footer arrow scroll functionality */
+  $('#scroll-to-work').click(() => {
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $('#work').offset().top,
+    }, 1100);
+  });
+  $('#scroll-to-blog').click(() => {
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $('#blog').offset().top,
+    }, 1100);
+  });
+  $('#scroll-to-social').click(() => {
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $('#social').offset().top,
+    }, 1100);
+  });
+  $('#scroll-to-top').click(() => {
+    $('html, body').animate({ scrollTop: 0 }, 1250);
+  });
+
+  /* Initial icon column slide-outs */
+  $(window).on('scroll', () => {
+    revealOnScroll('#work-slider');
+  });
+  $(window).on('scroll', () => {
+    revealOnScroll('#blog-slider');
+  });
+  $(window).on('scroll', () => {
+    revealOnScroll('#social-slider');
+  });
+
+  /* Hover icon column slide-out */
+  $('div.slider').hover(
+    function slideOut() { reveal(this); },
+    function slideIn() { hide(this); },
+  );
+});
