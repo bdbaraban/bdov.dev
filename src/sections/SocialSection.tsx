047c8397d23cd074d02eac631564da9470b75eb7
bdbaraban <375@holbertonschool.com>
2019-08-17 15:50:45 -0700

    Swap in latest tweet highlighting portfolio

--- src/sections/SocialSection.tsx
+++ src/sections/SocialSection.tsx
@@ -35,48 +35,40 @@ const SocialSection = (): ReactElement => {
     {
       content: (
         <h4 className="content">
-          &quot;Redesigned, re-introducing{' '}
-          <a href="https://bdov.dev" target="_blank" rel="noopener noreferrer">
-            bdov.dev
-          </a>{' '}
-          2.0. Shout-out to{' '}
+          &quot;Finished another iteration of my portfolio site. My React skills
+          have come a long way - pulled off a complete conversion onto{' '}
           <a
-            href="https://twitter.com/jgthms"
+            href="https://twitter.com/gatsbyjs"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="Jeremy Thomas Twitter"
           >
-            @jgthms
+            @gatsbyjs
           </a>{' '}
-          for Bulma, my new favorite CSS framework, and{' '}
-          <a
-            href="https://twitter.com/zeithq"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Zeit HQ Twitter"
-          >
-            @zeithq
-          </a>{' '}
-          for making deployment so, so simple.{' '}
+          in less than a week. It&apos;s all about those incremental
+          improvements.{' '}
           <a href="#" tabIndex={-1}>
             #portfolio
+          </a>{' '}
+          <a href="#" tabIndex={-1}>
+            #react
+          </a>{' '}
+          <a href="#" tabIndex={-1}>
+            #bulmacss
+          </a>{' '}
+          <a href="#" tabIndex={-1}>
+            #zeitnow
+          </a>
+          <br />
+          <br />
+          Check it out!{' '}
+          <a href="https://bdov.dev" target="_blank" rel="noopener noreferrer">
+            bdov.dev
           </a>
           &quot;
         </h4>
       ),
-      date: new Date('2019-06-20'),
-      link: 'https://twitter.com/bdov_/status/1141831637497368576'
-    },
-    {
-      content: (
-        <h4 className="content">
-          &quot;I really like React. <a tabIndex={-1}>#React</a>{' '}
-          <a tabIndex={-1}>#JavaScript</a> <a tabIndex={-1}>#TypeScript</a>
-          &quot;
-        </h4>
-      ),
-      date: new Date('2019-06-06'),
-      link: 'https://twitter.com/bdov_/status/1136737122495238144'
+      date: new Date('2019-08-17'),
+      link: 'https://twitter.com/bdov_/status/1162858307683381248'
     },
     {
       content: (
@@ -90,6 +82,17 @@ const SocialSection = (): ReactElement => {
       ),
       date: new Date('2019-05-21'),
       link: 'https://twitter.com/bdov_/status/1130893871045464064'
+    },
+    {
+      content: (
+        <h4 className="content">
+          &quot;I really like React. <a tabIndex={-1}>#React</a>{' '}
+          <a tabIndex={-1}>#JavaScript</a> <a tabIndex={-1}>#TypeScript</a>
+          &quot;
+        </h4>
+      ),
+      date: new Date('2019-06-06'),
+      link: 'https://twitter.com/bdov_/status/1136737122495238144'
     },
     {
       content: (
