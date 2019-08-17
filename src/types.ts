3666c1a6fcf88a1dd5dea87d9a4e4dda953a638f
bdbaraban <375@holbertonschool.com>
2019-08-16 18:40:14 -0700

    Upload dedicated types file declaring commonly-used icon link, project tile, blog tile and social tile object types

---
+++ src/types.ts
@@ -0,0 +1,61 @@
+import { ReactElement } from 'react';
+
+/**
+ * Link information for clickable icons
+ */
+export interface IconLink {
+  // Link
+  href: string;
+  // Icon label
+  label: string;
+  // Icon element
+  element: ReactElement;
+}
+
+/**
+ * Personal project information for project tiles
+ */
+export interface ProjectTileProps {
+  // Name of project
+  title: string;
+  // Project emoji (displayed with title)
+  emoji: ReactElement;
+  // Link to deployed project (optional)
+  link?: string;
+  // Link to project GitHub page
+  github: string;
+  // Project description
+  description: string;
+  // Array of tools used in project
+  tools: string[];
+}
+
+/**
+ * Blog article information for project tiles
+ */
+export interface BlogTileProps {
+  // Article title
+  title: string;
+  // Article description
+  description: string;
+  // Link to article
+  link: string;
+  // Article posted date
+  date: Date;
+  // Read time
+  minutes: number;
+  // Medium clap count (optional - if Medium article)
+  claps?: number;
+}
+
+/**
+ * Social post information for project tiles
+ */
+export interface SocialTileProps {
+  // Link to post
+  link: string;
+  // Post content
+  content: ReactElement;
+  // Posted date
+  date: Date;
+}
