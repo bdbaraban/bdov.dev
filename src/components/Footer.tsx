5e827b27b6878c6b39e3088140eaeb60eb62de88
bdbaraban <375@holbertonschool.com>
2019-09-02 14:14:50 -0700

    Convert to space-separated content including a link to site GitHub page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,19 @@
 import React, { ReactElement } from 'react';
 import styled from 'styled-components';
+import { FaGithub } from 'react-icons/fa';
 
-// Custom styled container
-const StyledDiv = styled.div`
-  max-width: 80%;
+// Custom styled p element
+const StyledP = styled.p`
+  align-items: center;
+  flex-direction: row;
+  display: flex;
+  justify-content: space-between;
+`;
+
+// Custom styled p element
+const StyledA = styled.a`
+  display: flex;
+  padding-left: 7px;
 `;
 
 /**
@@ -11,40 +21,49 @@ const StyledDiv = styled.div`
  */
 const Footer = (): ReactElement => {
   return (
-    <footer className="footer">
-      <StyledDiv className="content has-text-centered has-text-white">
-        <p>
-          Made with{' '}
-          <a
-            className="has-text-warning"
-            href="https://www.gatsbyjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GatsbyJS
-          </a>{' '}
-          and{' '}
-          <a
-            className="has-text-warning"
-            href="https://bulma.io/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Bulma CSS
-          </a>
-          .
-          <br /> Deployed on{' '}
-          <a
-            className="has-text-warning"
-            href="https://zeit.co/now"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ZEIT Now
-          </a>
-          .
-        </p>
-      </StyledDiv>
+    <footer className="footer content is-small has-text-white has-text-centered">
+      <p>
+        Made with{' '}
+        <a
+          className="has-text-warning"
+          href="https://www.gatsbyjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GatsbyJS
+        </a>{' '}
+        and{' '}
+        <a
+          className="has-text-warning"
+          href="https://bulma.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bulma CSS
+        </a>
+        .
+        <br /> Deployed on{' '}
+        <a
+          className="has-text-warning"
+          href="https://zeit.co/now"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ZEIT Now
+        </a>
+        .
+      </p>
+      <StyledP>
+        bdov.dev{' '}
+        <StyledA
+          href="https://github.com/bdbaraban/bdov.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="bdov.dev GitHub"
+        >
+          <FaGithub className="footer-icon" />
+        </StyledA>
+      </StyledP>
     </footer>
   );
 };
