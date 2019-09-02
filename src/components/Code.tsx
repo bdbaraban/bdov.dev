97900a3e5643329f051c0cddd6f820805ae83c02
bdbaraban <375@holbertonschool.com>
2019-09-01 17:30:03 -0700

    Remove wrapping of <pre> block with <div>

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -6,55 +6,53 @@ import Typist from 'react-typist';
  */
 const Code = (): ReactElement => {
   return (
-    <div className="level-item">
-      <pre className="code-highlight">
-        <Typist>
-          <span className="keyword">const</span>{' '}
-          <span className="punctuation">{'['}</span>
-          me, setMe
-          <span className="punctuation">{']'}</span> ={' '}
-          <span className="keyword">React</span>.
-          <span className="function">useState</span>
-          <span className="punctuation">{'('}</span>
-          <span className="symbol">{'{}'}</span>
-          <span className="punctuation">{')'}</span>
-          {';'}
-          <br />
-          <br />
-          <span className="function">setMe</span>
-          <span className="punctuation">{'('}</span>
-          <span className="symbol">{'{'}</span>
-          <br />
-          {'  '}
-          what: <span className="string">&apos;Software Engineer&apos;</span>
-          {','}
-          <br />
-          {'  '}
-          where:{' '}
-          <span className="string">&apos;San Francisco Bay Area&apos;</span>
-          {', '}
-          <br />
-          {'  '}
-          alumni: <span className="punctuation">{'['}</span>
-          <span className="string">&apos;UC Davis&apos;</span>
-          {', '}
-          <span className="string">&apos;Holberton School&apos;</span>
-          <span className="punctuation">{']'}</span>
-          {', '}
-          <br />
-          {'  '}
-          loves: <span className="string">&apos;React&apos;</span>
-          {', '}
-          <br />
-          {'  '}
-          alwaysLearning: <span className="boolean">true</span>
-          <br />
-          <span className="symbol">{'}'}</span>
-          <span className="punctuation">{')'}</span>
-          {';'}
-        </Typist>
-      </pre>
-    </div>
+    <pre className="code-highlight is-centered">
+      <Typist>
+        <span className="keyword">const</span>{' '}
+        <span className="punctuation">{'['}</span>
+        me, setMe
+        <span className="punctuation">{']'}</span> ={' '}
+        <span className="keyword">React</span>.
+        <span className="function">useState</span>
+        <span className="punctuation">{'('}</span>
+        <span className="symbol">{'{}'}</span>
+        <span className="punctuation">{')'}</span>
+        {';'}
+        <br />
+        <br />
+        <span className="function">setMe</span>
+        <span className="punctuation">{'('}</span>
+        <span className="symbol">{'{'}</span>
+        <br />
+        {'  '}
+        what: <span className="string">&apos;Software Engineer&apos;</span>
+        {','}
+        <br />
+        {'  '}
+        where:{' '}
+        <span className="string">&apos;San Francisco Bay Area&apos;</span>
+        {', '}
+        <br />
+        {'  '}
+        alumni: <span className="punctuation">{'['}</span>
+        <span className="string">&apos;UC Davis&apos;</span>
+        {', '}
+        <span className="string">&apos;Holberton School&apos;</span>
+        <span className="punctuation">{']'}</span>
+        {', '}
+        <br />
+        {'  '}
+        loves: <span className="string">&apos;React&apos;</span>
+        {', '}
+        <br />
+        {'  '}
+        alwaysLearning: <span className="boolean">true</span>
+        <br />
+        <span className="symbol">{'}'}</span>
+        <span className="punctuation">{')'}</span>
+        {';'}
+      </Typist>
+    </pre>
   );
 };
 
