6834373a6b175edb3840eb71f6e62268f3332851
bdbaraban <375@holbertonschool.com>
2019-09-02 21:02:37 -0700

    Do not add one to getMonth() call, improve spacing

--- src/utils/getDateString.ts
+++ src/utils/getDateString.ts
@@ -1,9 +1,11 @@
 import { MONTHS } from 'utils/constants';
+
 /**
  * Convert a Date to a render-able string.
  * @param date - The date to convert.
  */
-const getDateString = (date: Date): string =>
-  `${MONTHS[date.getMonth()]} ${date.getDate() + 1}, ${date.getFullYear()}`;
+const getDateString = (date: Date): string => {
+  return `${MONTHS[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+};
 
 export default getDateString;
