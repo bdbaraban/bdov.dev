cdf6df5c16d116fc68bac712a425abc2f0dacaeb
bdbaraban <375@holbertonschool.com>
2019-09-03 00:02:12 -0700

    Remove passing of unused props

--- __mocks__/gatsby.js
+++ __mocks__/gatsby.js
@@ -4,23 +4,11 @@ const gatsby = jest.requireActual('gatsby');
 module.exports = {
   ...gatsby,
   graphql: jest.fn(),
-  Link: jest.fn().mockImplementation(
-    // these props are invalid for an `a` tag
-    ({
-      activeClassName,
-      activeStyle,
-      getProps,
-      innerRef,
-      partiallyActive,
-      ref,
-      replace,
-      to,
+  Link: jest.fn().mockImplementation(({ to, ...rest }) =>
+    React.createElement('a', {
+      href: to,
       ...rest
-    }) =>
-      React.createElement('a', {
-        ...rest,
-        href: to
-      })
+    })
   ),
   StaticQuery: jest.fn(),
   useStaticQuery: jest.fn()
