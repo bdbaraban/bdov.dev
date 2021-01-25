bf901e847a02217b24a53fea018717121c8c481f
bdbaraban <brennan@bdov.dev>
2021-01-24 20:27:00 -0800

    Include favicon files with build to support emoji use on Safari

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -29,6 +29,27 @@ const SEO = () => {
       <html lang="en" />
       <title>{site.siteMetadata.title}</title>
       <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link
+        rel="mask-icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦙</text></svg>"
+      />
+      <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦙</text></svg>"
       />
